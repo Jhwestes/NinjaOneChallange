6a01db87e7b853f83da7cca6395a1b7f444e4f78
ldominguez <jhwestesluisf@gmail.com>
2024-08-22 22:55:32 -0600

    upgrade test coverage, add comments

--- testcafe/tests.js
+++ testcafe/tests.js
@@ -1,7 +1,7 @@
 import { Selector, t } from 'testcafe';
 import Page from './page-model';
 
-fixture .page`http://localhost:3001` `DEVICE PAGE TESTS`
+fixture .page`http://localhost:3001/` `DEVICE PAGE TESTS`   // Before each test open this URL
 
 test
 (`Device List is displayed correctly`, async t => {
@@ -10,17 +10,29 @@ test
 
 test
 (`New Device is added as expected`, async t => {
-    await t.click(Page.addDeviceButton);
+    await t.click(Page.addDeviceButton);                    
     await Page.fillDeviceInfo('Test', 'MAC', '100');
-    await Page.compareDeviceVsList('Test', 'MAC', '100');
+    await Page.compareDeviceVsList('Test', 'MAC', '100');   
 });
 
 test
-(`Update a device name with API call`, async t => {
-    await Page.updateFirstDevice();
+(`Update first device in list name with API call`, async t => {
+    await t
+        .click(Page.editButton.nth(0))
+        .wait(1000);
+    await Page.updateDevice();
 });
 
 test
-(`Delete a device name with API call`, async t => {
-    await Page.deleteLastDevice();
+(`Delete last device in list with API call`, async t => {
+    const deletedName = await Page.deviceName.nth(-1).innerText;
+    
+    await t
+        .click(Page.editButton.nth(-1))
+        .wait(1000);
+    await Page.deleteDevice();
+
+    const currentName = await Page.deviceName.withText(deletedName).visible;        // check if deleted device is visible (T/F)
+    await t.expect(currentName).notOk(currentName + 'is still visible');            // expect visibility status to be FALSE
+    console.log(deletedName + 'was deleted successfully');
 });
